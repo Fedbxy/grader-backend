be9c38db0023ce3aa5c2e25ea1c62ace21675d53
Punnaphop Sitthiamornporn <punnaphop.sitthi@gmail.com>
2024-09-09 13:49:01 +0700

    feat: add compile for python

--- src/config/command.py
+++ src/config/command.py
@@ -2,7 +2,7 @@ def compile(isolatePath: str, id: int, language: str):
     commands = {
         "cpp": ["/usr/bin/g++", "-std=c++17", "-O2", f"{isolatePath}/{id}.cpp", "-o", f"{isolatePath}/{id}"],
         "c": ["/usr/bin/gcc", "-std=c11", "-O2", f"{isolatePath}/{id}.c", "-o", f"{isolatePath}/{id}"],
-        "py": ["true"],
+        "py": ["/usr/local/bin/python3", "-m", "py_compile", f"{isolatePath}/{id}.py"],
     }
 
     return commands[language]
